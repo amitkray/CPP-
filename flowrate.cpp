3d8b17bc077c80faded2867be353b733081e38a9
amitkray <amitkray@gmail.com>
2020-01-28 12:54:54 +0530

    Add files via upload

---
+++ flowrate.cpp
@@ -0,0 +1,11 @@
+#include "flowrate.h"
+
+flowrate::flowrate(Ps,Ga,Vs,Vi,Hl,Sj,Fr,As)
+{
+    //ctor
+}
+
+flowrate::~flowrate()
+{
+    //dtor
+}
---
+++ frictionfactor.cpp
@@ -0,0 +1,11 @@
+#include "frictionfactor.h"
+
+frictionfactor::frictionfactor(Re,e,d)
+{
+    //ctor
+}
+
+frictionfactor::~frictionfactor()
+{
+    //dtor
+}
